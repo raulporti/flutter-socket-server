01fb8a540d15465b11ffdf63599844427692c807
Raul Portillo <raulportillo1610@gmail.com>
2021-06-15 09:46:33 -0600

    first Commit

---
+++ sockets/sockets.js
@@ -0,0 +1,13 @@
+const { io } = require('../index')
+io.on('connection', client => {
+    console.log('cliente conectado')
+    //client.on('event', data => { });
+    client.on('disconnect', () => {
+        console.log('cliente desconectado')
+    })
+
+    client.on('mensaje', (payload) => {
+        console.log('Mensaje!!', payload)
+        io.emit('mensaje', {admin: 'Nuevo Mensaje'})
+    })
+})
